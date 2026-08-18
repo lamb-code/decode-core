c9a4f66907bfa29b778003a362d908f674985bde
Tauger <5293303+tauger@user.noreply.gitee.com>
2026-08-18 15:06:45 +0800

    实现对数组的方法劫持

---
+++ vue2.0/src/observe/array.js
@@ -0,0 +1,33 @@
+//获取数组原来的方法
+let oldArrayProto = Array.prototype; // 获取数组原型
+//我们希望数组原来的方法都还存在就拷贝一份出来
+
+//newArrayProto.__proto__=oldArrayProto
+export let newArrayProto = Object.create(oldArrayProto);
+let methods = ["push", "shift", "pop", "unshift", "reverse", "sort", "splice"];
+methods.forEach((method) => {
+  newArrayProto[method] = function (...args) {
+    const result = oldArrayProto[method].call(this, ...args);
+    //对新增的数据再次进行劫持
+    // 可以添加自己逻辑，函数劫持，切片
+    let inserted = [];
+    let ob = this.__ob__;
+    switch (method) {
+      case "splice": // 修改 删除 添加
+        inserted = args.slice(2); // splice 方法从第三个参数起，是增添的新数据
+        break;
+      case "push":
+      case "unshift":
+        inserted = args;
+        break;
+    }
+    // inserted[] 遍历数组 看一下他是否需要进行二次劫持
+    if (inserted) {
+      //如果插入的是对象数组 还需要进行劫持
+      //怎么拿到observeArray呢？唯一能拿到是this
+      ob.observeArray(inserted);
+    }
+    // ob.dep.notify(); // 触发页面更新流程
+    return result;
+  };
+});
--- vue2.0/src/observe/index.js
+++ vue2.0/src/observe/index.js
@@ -1,12 +1,32 @@
+import { newArrayProto } from "./array";
+
 class Observer {
   constructor(data) {
-    // 如果给一个对象添加一个不存在的属性，我希望也能更新视图{}.dep Object. 只能劫持已经存在的属性(vue内部会单独写一些api $set $delete)
-    this.walk(data);
+    // data.__ob__ = this // 给对象和数组添加一个自定义属性,为了重写array数组方法拿到observeArray方法且加了个标识区分数据是否被劫持过，但是直接这样会造成死循环，需要不能枚举属性
+    Object.defineProperty(data,'__ob__',{
+      value:this,
+      enumerable:false //表示这个属性不能被列举出来，不能被循环到
+    })
+    if (Array.isArray(data)) {
+      // 更改数组原型方法, 如果是数组,就重写数组的原型链
+      data.__proto__ = newArrayProto
+      this.observeArray(data)
+    } else {
+      // 如果给一个对象添加一个不存在的属性，我希望也能更新视图{}.dep Object. 只能劫持已经存在的属性(vue内部会单独写一些api $set $delete)
+      this.walk(data);
+    }
   }
   walk(data) {
     Object.keys(data).forEach((key) => {
       // 使用defineProperty重新定义属性(之所以vue2性能较3差)
       defineReactive(data, key, data[key]); //defineReactive不写在内部，方便后面可以单独使用
+    });
+  }
+  observeArray(data) {
+    // 递归遍历数组,对数组内部的对象再次重写[[]] [{}]
+    data.forEach((item) => {
+      // 数组里如果是引用类型,那么是响应式
+      observe(item);
     });
   }
 }
@@ -24,6 +44,7 @@ export function defineReactive(target, key, value) {
     //设置值的时候执行set
     set(newValue) {
       if (newValue === value) return;
+      observe(value); //为什么这还需要observe 防止 vm.object1={ num:2} 直接赋值
       value = newValue;
     },
   });
@@ -33,6 +54,8 @@ export function observe(data) {
   if (typeof data !== "object" || data == null) {
     return;
   }
+  if (value.__ob__) return // 一个对象不需要重新被观测
+
   // 2.需要对 对象进行观测, 最外层必须是一个{}, 不能是数组
   // 如果一个数据已经被观测过了, 就不要在进行观测了, 用类来实现, 观测过就增加一个标识,再观测的时候,可以先检测是否观测过,观测过了就跳过检测
   return new Observer(data);
