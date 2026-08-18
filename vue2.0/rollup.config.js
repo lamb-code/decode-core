945b9a0b1129e504c520a05f604c0ab78b016403
Tauger <5293303+tauger@user.noreply.gitee.com>
2026-08-18 08:42:53 +0800

    rollup的环境配置

---
+++ vue2.0/rollup.config.js
@@ -0,0 +1,17 @@
+import babel from "rollup-plugin-babel";
+export default {
+  input: "./src/index.js", //打包入口文件
+  output: {
+    file: "dist/umd/vue.js", //打包出口文件
+    name: "Vue", //会在global 添加 Vue属性 global.vue
+    format: "umd", //打包格式umd,常见格式 esm es6模块 commonjs 模块
+    sourcemap: true, //希望可以调试源代码
+  },
+  plugins: [
+    // babel 默认会取.babelrc 配置项
+    babel({
+      exclude: "node_modules/**", // glob写法，去掉依赖库下的所有文件夹下的文件
+    }),
+  ],
+};
+// 为什么Vue2 只能支持IE9.0以上，原因是Object.defineProperty不支持低版本的
