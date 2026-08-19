755ebd25d18d46e3f8a6f2177d63faf2b0ec0138
Tauger <5293303+tauger@user.noreply.gitee.com>
2026-08-19 20:41:12 +0800

    getters的实现

--- vuex-router-2.x/src/vuex/store.js
+++ vuex-router-2.x/src/vuex/store.js
@@ -1,5 +1,7 @@
 /* eslint-disable*/
 import appllyMixin from "./mixin";
+import { forEachValue } from "./util";
+
 let Vue;
 //  最终用户拿到是这个实例
 
@@ -8,20 +10,38 @@ class Store {
   constructor(options) {
     // this.state = options.state; //如果直接state定义在实例上，稍后这个状态发生变化 视图是不会更新的，vue-router 是通过defineReactive 创建响应式数据
     let state = options.state;
+    // this.getters = options.getters //getters 其实写的是方法，但是取值的时候是属性,所以用到defineProperty
+    this.getters = {};
+    const computed = {};
+    /*
+     每次更新都会执行,考虑到缓存效果都放在computed里
+    forEachValue(options.getters,(fn,key)=>{
+      Object.defineProperty(this.getters,key,{
+        get:()=>fn(this.state)
+      })
+    })
+    */
+    forEachValue(options.getters, (fn, key) => {
+      computed[key] = () => {
+        return fn(this.state);
+      };
+      Object.defineProperty(this.getters, key, {
+        // get: () => fn(this.state),
+        get: () => this._vm[key],//也就是取的vue实例计算属性
+      });
+    });
+
     //vuex则是创建个vue实例
     this._vm = new Vue({
       data: {
         $$state: state, //为什么两个$$?vue 中定义数据属性名是有特点的，如果属性名是通过 $xxx 命名的 他不会被代理到vue实例上。
       },
+      computed,//计算属性会降自己的属性放到实例上
     });
   }
   // 用户怎么拿数据？ 通过类属性访问器，当用户去这个实例上取states属性时会执行此方法
   get state() {
     return this._vm.data.$$state;
-  }
-
-  get state() {
-    return this._vm._data.$$state;
   }
 }
 const install = (_Vue) => {
