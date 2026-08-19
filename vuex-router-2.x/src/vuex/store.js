b3f5bbe150ab40d5efc16f379f3a39dfca2a7259
Tauger <5293303+tauger@user.noreply.gitee.com>
2026-08-19 20:56:39 +0800

    mutations和actions的实现

--- vuex-router-2.x/src/vuex/store.js
+++ vuex-router-2.x/src/vuex/store.js
@@ -27,7 +27,7 @@ class Store {
       };
       Object.defineProperty(this.getters, key, {
         // get: () => fn(this.state),
-        get: () => this._vm[key],//也就是取的vue实例计算属性
+        get: () => this._vm[key], //也就是取的vue实例计算属性
       });
     });
 
@@ -36,8 +36,30 @@ class Store {
       data: {
         $$state: state, //为什么两个$$?vue 中定义数据属性名是有特点的，如果属性名是通过 $xxx 命名的 他不会被代理到vue实例上。
       },
-      computed,//计算属性会降自己的属性放到实例上
+      computed, //计算属性会降自己的属性放到实例上
     });
+
+    //将用户的mutation，action 先保存起来 稍后当调用 commit时 就找订阅的mutation方法 调用dispatch 就找对应的action方法 采用的是发布订阅模式
+    this._mutations = {};
+    forEachValue(options.mutations, (fn, type) => {
+      this._mutations[type] = (payload) => {
+        fn.call(this, this.state, payload);
+      };
+    });
+
+    this._acitons = {};
+    forEachValue(options.actions, (fn, type) => {
+      this._acitons[type] = (payload) => {
+        fn.call(this, this, payload);
+      };
+    });
+  }
+  commit = (type, payload) => {
+    //这需要考虑this问题，为了方便用箭头函数，为什么会有this问题，因为aciton参数可以解构{commit,store}
+    this._mutations[type](payload);
+  };
+  dispatch(type, payload) {
+    this._acitons[type](payload);
   }
   // 用户怎么拿数据？ 通过类属性访问器，当用户去这个实例上取states属性时会执行此方法
   get state() {
