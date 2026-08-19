673976a82a56be19283a811d50d990190d0b5496
Tauger <5293303+tauger@user.noreply.gitee.com>
2026-08-19 11:54:18 +0800

    Reflect的使用

---
+++ vue3.x-ts/packages/reactivity/src/baseHandler.ts
@@ -0,0 +1,28 @@
+export enum ReactiveFlags {
+  IS_REACTIVE = "__v_isReactive",
+  IS_READONLY = "__v_isReadonly",
+  RAW = "__v_raw",
+}
+
+export const mutableHandlers: ProxyHandler<any> = {
+  get(target, key, recevier) {
+    /*
+     *判断是否代理过了，代理过了就直接取值:
+     * let obj={name:'jane'}
+     * const state1=reactive(obj)
+     * const state2=reactive(state1)
+     * consloe.log(state1===state2) =>false
+     * 在reactive方法尝试取IS_REACTIVE属性如果存在就说明已经代理过了
+     */
+    if (key === ReactiveFlags.IS_REACTIVE) {
+      return true;
+    }
+    // return target[key];// 不能直接返回 target[key]
+    return Reflect.get(target,key,recevier) //proxy 需要搭配reflect来使用
+    //当取值的时候 应该让响应式属性和effect 映射起来
+  },
+  set(target, key, value, recevier) {
+    return Reflect.set(target,key,value,recevier)
+
+  },
+};
--- vue3.x-ts/packages/reactivity/src/reactive.ts
+++ vue3.x-ts/packages/reactivity/src/reactive.ts
@@ -1,4 +1,5 @@
 import { isObject } from "@vue/shared";
+import { ReactiveFlags, mutableHandlers } from "./baseHandler";
 
 /*
  *reactiveMap用于记录我们的代理后的结果可以复用如:
@@ -9,33 +10,6 @@ import { isObject } from "@vue/shared";
  */
 const reactiveMap = new WeakMap();
 
-enum ReactiveFlags {
-  IS_REACTIVE = "__v_isReactive",
-  IS_READONLY = "__v_isReadonly",
-  RAW = "__v_raw",
-}
-
-const mutableHandlers: ProxyHandler<any> = {
-  get(target, key, recevier) {
-    /*
-     *判断是否代理过了，代理过了就直接取值:
-     * let obj={name:'jane'}
-     * const state1=reactive(obj)
-     * const state2=reactive(state1)
-     * consloe.log(state1===state2) =>false
-     * 在reactive方法尝试取IS_REACTIVE属性如果存在就说明已经代理过了
-     */
-    if (key === ReactiveFlags.IS_REACTIVE) {
-      return true;
-    }
-  },
-  set(target, key, value, recevier) {
-    return true;
-  },
-};
-export function reactive(target) {
-  return createReactiveObject(target);
-}
 function createReactiveObject(target) {
   if (!isObject(target)) {
     return target;
@@ -49,4 +23,7 @@ function createReactiveObject(target) {
   const proxy = new Proxy(target, mutableHandlers);
   reactiveMap.set(target, proxy); //根据对象缓存代理后的结果
   return proxy;
+}
+export function reactive(target) {
+  return createReactiveObject(target);
 }
