9c41f0d772be4e7c90ed7a0034102798d236dffb
Tauger <5293303+tauger@user.noreply.gitee.com>
2026-08-20 16:50:54 +0800

    定义$route和$router

--- vuex-router-2.x/src/vue-router/install.js
+++ vuex-router-2.x/src/vue-router/install.js
@@ -9,24 +9,25 @@ export default function install(Vue) {
       if (this.$options.router) {
         this._routerRoot = this; //这是将当前根实例放到了_routerRoot属性上，为的是让子也拿到这个根实例
         this._router = this.$options.router; //把VueRouter实例定义到vue根实例_router 为的是让子组件通过this._routerRoot._router 可以拿到VueRouter实例
-        this._router.init(this);//初始化路由表
+        this._router.init(this); //初始化路由表
         //如果用户更改current 是没有效果的 需要把_route进行更新，怎么处理? 在base.js 增加listen 方法接收个回调
-        Vue.util.defineReactive(this, "_route", this._router.history.current);//把current 设置到当前根实例_route属性上 具有响应式
+        Vue.util.defineReactive(this, "_route", this._router.history.current); //把current 设置到当前根实例_route属性上 具有响应式
       } else {
         this._routerRoot = this.$parent && this.$parent._routerRoot; //一直往上找 获取到根实例
       }
     },
   });
-  // Object.defineProperty(Vue.prototype, "$route", {
-  //   get() {
-  //     return this._routerRoot._route;
-  //   },
-  // });
-  // Object.defineProperty(Vue.prototype, "$router", {
-  //   get() {
-  //     return this._routerRoot._router;
-  //   },
-  // });
+  //代理$route $router属性 让每个组件都有这两个属性
+  Object.defineProperty(Vue.prototype, "$route", {
+    get() {
+      return this._routerRoot._route;
+    },
+  });
+  Object.defineProperty(Vue.prototype, "$router", {
+    get() {
+      return this._routerRoot._router;
+    },
+  });
   //注册全局属性 $route $router
   //注册全局指令 v-scroll
   //注册全局组件 router-view router-link
