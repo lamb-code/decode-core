828c525bcd6eded859ea9213132b626fff9124a6
Tauger <5293303+tauger@user.noreply.gitee.com>
2026-08-20 11:33:01 +0800

    【VueRouter】给所有组件定义属性

--- vuex-router-2.x/src/main.js
+++ vuex-router-2.x/src/main.js
@@ -2,12 +2,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-// import store from './store'
+import store from './store'
 Vue.config.productionTip = false
-console.log(Vue.options)
+// console.log(Vue.options)
 const vm = new Vue({
-  router,
-  // store,
+  router, //router为什么不能放在原型上？防止其他重新new Vue也会拿到router,这里只是让所有子组件都可以获取router属性
+  store,
   render: h => h(App)
 }).$mount('#app')
 console.log(vm)
+// 手写路由分析：
+// 前端路由有常见的两个方案hash的模式 #aaa #bbb；浏览器的历史记录是一个栈结构
---
+++ vuex-router-2.x/src/vue-router/install.js
@@ -0,0 +1,33 @@
+/* eslint-disable */
+import RouterView from "./components/view";
+export default function install(Vue) {
+  // Vue就是Vue的构造函数
+  Vue.mixin({
+    beforeCreate() {
+      //如果有router属性说明根实例增加了router 当前这个实例是根实例
+      //页面渲染流程是先父后子 渲染完毕是先子后父
+      if (this.$options.router) {
+        this._routerRoot = this; //这是将当前根实例放到了_routerRoot属性上，为的是让子也拿到这个根实例
+        this._router = this.$options.router; //把VueRouter实例定义到vue根实例_router 为的是让子组件通过this._routerRoot._router 可以拿到VueRouter实例
+        // this._router.init(this);
+        // Vue.util.defineReactive(this, "_route", this._router.history.current);
+      } else {
+        this._routerRoot = this.$parent && this.$parent._routerRoot; //一直往上找 获取到根实例
+      }
+    },
+  });
+  // Object.defineProperty(Vue.prototype, "$route", {
+  //   get() {
+  //     return this._routerRoot._route;
+  //   },
+  // });
+  // Object.defineProperty(Vue.prototype, "$router", {
+  //   get() {
+  //     return this._routerRoot._router;
+  //   },
+  // });
+  //注册全局属性 $route $router
+  //注册全局指令 v-scroll
+  //注册全局组件 router-view router-link
+  Vue.component("RouterView", RouterView);
+}
