4f2a6a52787fa592f86e6649ab257c2852f36b43
Tauger <5293303+tauger@user.noreply.gitee.com>
2026-08-23 18:40:55 +0800

    setup入口实现

--- vue3.x-ts/packages/runtime-core/src/component.ts
+++ vue3.x-ts/packages/runtime-core/src/component.ts
@@ -1,4 +1,4 @@
-import { reactive } from "@vue/reactivity";
+import { proxyRefs, reactive } from "@vue/reactivity";
 import { hasOwn, isFunction } from "@vue/shared";
 
 export function createComponentInstance(vnode) {
@@ -13,6 +13,7 @@ export function createComponentInstance(vnode) {
     propsOptions: vnode.type.props, // 用户声明的那些属性是组件的属性
     component: null,
     proxy: null, //用来代理 props attrs data 让用户方便的取值
+    setupState: null,
   };
   return instance;
 }
@@ -39,18 +40,20 @@ const publicProperty = {
 };
 const handler = {
   get(target, key) {
-    const { data, props } = target;
+    const { data, props,setupState } = target;
     if (data && hasOwn(data, key)) {
       return data[key];
     } else if (props && hasOwn(props, key)) {
       return props[key];
+    }else if(setupState&&hasOwn(setupState,key)){
+      return setupState[key]
     }
     const getter = publicProperty[key];
     return getter && getter(target);
   },
   // 对于一些属性无法修改的属性如 $slot $attrs... 那就去实例上取
   set(target, key, value) {
-    const { data, props } = target;
+    const { data, props,setupState } = target;
     if (data && hasOwn(data, key)) {
       data[key] = value;
     } else if (props && hasOwn(props, key)) {
@@ -58,6 +61,8 @@ const handler = {
       // props[key] = value;
       console.warn("props is readonly");
       return false;
+    }else if(setupState&&hasOwn(setupState,key)){
+      setupState[key] = value;
     }
     return true;
   },
@@ -67,11 +72,23 @@ export function setupComponent(instance) {
   //赋值属性
   initProps(instance, vnode.props);
   //赋值代理对象
-  console.log(instance);
-
   instance.proxy = new Proxy(instance, handler);
-  const { data = () => {}, render } = vnode.type;
+  const { data = () => {}, render, setup } = vnode.type;
+
+  if (setup) {
+    const setupContext = {};
+    const setupResult = setup(instance.props, setupContext);
+    if (isFunction(setupResult)) {
+      instance.render = setupResult;
+    } else {
+      instance.setupState = proxyRefs(setupResult);
+    }
+  }
+
   if (!isFunction(data)) return console.warn("function");
   instance.data = reactive(data.call(instance.proxy));
-  instance.render = render;
+  if (!instance.render) {
+    instance.render = render;
+  }
+  //instance.render = render;
 }
