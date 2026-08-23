e74fdbf599b3743346bd3da20f8a6167f7a15578
Tauger <5293303+tauger@user.noreply.gitee.com>
2026-08-23 19:06:16 +0800

    slots插槽实现

--- vue3.x-ts/packages/runtime-core/src/component.ts
+++ vue3.x-ts/packages/runtime-core/src/component.ts
@@ -1,5 +1,5 @@
 import { proxyRefs, reactive } from "@vue/reactivity";
-import { hasOwn, isFunction } from "@vue/shared";
+import { ShapeFlags, hasOwn, isFunction } from "@vue/shared";
 
 export function createComponentInstance(vnode) {
   const instance = {
@@ -10,6 +10,7 @@ export function createComponentInstance(vnode) {
     update: null,
     props: {},
     attrs: {},
+    slots: {},
     propsOptions: vnode.type.props, // 用户声明的那些属性是组件的属性
     component: null,
     proxy: null, //用来代理 props attrs data 让用户方便的取值
@@ -35,25 +36,33 @@ const initProps = (instance, rawProps) => {
   instance.attrs = attrs;
   instance.props = reactive(props);
 };
+const initSlots = (instance, children) => {
+  if (instance.vnode.shapeFlag & ShapeFlags.SLOTS_CHILDREN) {
+    instance.slots = children;
+  } else {
+    instance.slots = {};
+  }
+};
 const publicProperty = {
   $attrs: (instance) => instance.attrs,
+  $slots: (instance) => instance.slots,
 };
 const handler = {
   get(target, key) {
-    const { data, props,setupState } = target;
+    const { data, props, setupState } = target;
     if (data && hasOwn(data, key)) {
       return data[key];
     } else if (props && hasOwn(props, key)) {
       return props[key];
-    }else if(setupState&&hasOwn(setupState,key)){
-      return setupState[key]
+    } else if (setupState && hasOwn(setupState, key)) {
+      return setupState[key];
     }
     const getter = publicProperty[key];
     return getter && getter(target);
   },
   // 对于一些属性无法修改的属性如 $slot $attrs... 那就去实例上取
   set(target, key, value) {
-    const { data, props,setupState } = target;
+    const { data, props, setupState } = target;
     if (data && hasOwn(data, key)) {
       data[key] = value;
     } else if (props && hasOwn(props, key)) {
@@ -61,7 +70,7 @@ const handler = {
       // props[key] = value;
       console.warn("props is readonly");
       return false;
-    }else if(setupState&&hasOwn(setupState,key)){
+    } else if (setupState && hasOwn(setupState, key)) {
       setupState[key] = value;
     }
     return true;
@@ -71,6 +80,8 @@ export function setupComponent(instance) {
   const { vnode } = instance;
   //赋值属性
   initProps(instance, vnode.props);
+  //初始化插槽
+  initSlots(instance, vnode.children);
   //赋值代理对象
   instance.proxy = new Proxy(instance, handler);
   const { data = () => {}, render, setup } = vnode.type;
