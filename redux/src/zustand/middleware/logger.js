776129f30ed69d9377c11e270f514bb5c9353238
Tauger <5293303+tauger@user.noreply.gitee.com>
2026-09-09 16:52:53 +0800

    zustand 实现

--- redux/src/zustand/middleware/logger.js
+++ redux/src/zustand/middleware/logger.js
@@ -1,6 +1,5 @@
 export function logger(createState) {
   return (set, get, api) => {
-    console.log(set,get,api,'ddd')
     return createState(
       (...args) => {
         console.log("老状态", get());
--- redux/src/zustand/react.js
+++ redux/src/zustand/react.js
@@ -1,12 +1,30 @@
 import { createStoreImpl } from "./vanilla";
-import { useSyncExternalStore } from "react";
-export function useStore(api) {
-  let value = useSyncExternalStore(api.subscribe, api.getState);
+import { useSyncExternalStore, useRef, useCallback } from "react";
+export function useStore(api, selector=(s)=>s) {
+  const lastSnapshotRef = useRef(null);
+  const lastSelectionRef = useRef(null);
+  const getSnapshot = useCallback(() => {
+    let lastSelection = lastSnapshotRef.current;
+    if (lastSelection === null) {
+      const nextSnapShot = api.getState();
+      const newSelection = selector(nextSnapShot);
+      lastSelectionRef.current = newSelection;
+      lastSnapshotRef.current = nextSnapShot;
+      return newSelection;
+    }else{
+        const lastSnapShot = lastSnapshotRef.current
+        const nextSnapShot = api.getState()
+        if(Object.is(lastSnapShot,nextSnapShot)){
+            return lastSelection
+        }
+    }
+  }, []);
+  let value = useSyncExternalStore(api.subscribe, getSnapshot);
   return value;
 }
 export const create = (createState) => {
   const api = createStoreImpl(createState);
-  return () => useStore(api);
+  return (selector) => useStore(api, selector);
 };
 
 export default create;
