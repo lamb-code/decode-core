1524df3b7aabe321b476e5fdf1f3578e0a311ced
Tauger <5293303+tauger@user.noreply.gitee.com>
2026-09-09 12:26:25 +0800

    实现中间件

--- redux/src/redux/applyMiddleware.js
+++ redux/src/redux/applyMiddleware.js
@@ -2,7 +2,12 @@ export default function applyMiddleware(logger) {
   return function (createStore) {
     return function (reducer) {
       const store = createStore(reducer);
-      const dispatch = logger(store)(store.dispatch);
+      let dispatch;
+      let middlewareAPI = {
+        getState: store.getState,
+        dispatch: (action) => dispatch(action),
+      };
+      dispatch = logger(middlewareAPI)(store.dispatch);
       return { ...store, dispatch };
     };
   };
